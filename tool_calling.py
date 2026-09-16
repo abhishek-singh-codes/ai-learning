e0beaad20e37183af3fd3a61334acb2faac871e5
abhishek-singh-codes <asingh19082000@gmail.com>
2026-09-16 22:11:47 +0530

    Tool calling decide by LLM but calling from application

--- tool_calling.py
+++ tool_calling.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+import json
 
 client = OpenAI()
 
@@ -35,6 +36,39 @@ response = client.responses.create(
     tools=tools
 )
 
+#(arguments='{"a":847,"b":293}', call_id='call_hV8V2kI1JUoDJh4jK8oyVY6p', name='multiply', type='function_call', id='fc_06051fb4c16e6342006aaac3457e5487d1ab75c4c6c08e1a98', caller=None, namespace=None, status='completed')
+
 
 for item in response.output:
-    print(item)
+
+    if item.type == "function_call":
+
+        function_name = item.name
+        arguments = json.loads(item.arguments)
+
+        print("GPT wants to call:", function_name)
+        print("Arguments:", arguments)
+
+        if function_name == "multiply":
+            result = multiply(
+                arguments["a"],
+                arguments["b"]
+            )
+
+            print("Tool result:", result)
+
+            tool_result = {
+                "type": "function_call_output",
+                "call_id": item.call_id,
+                "output": str(result)
+            }
+
+            final_response = client.responses.create(
+                model="gpt-5.6-luna",
+                previous_response_id=response.id,
+                input=[tool_result],
+                tools=tools
+            )
+
+            print("\nFinal answer:")
+            print(final_response.output_text)
